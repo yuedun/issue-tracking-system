384c91b0f1d2b575e63ae487f49910da1da82ea7
yuedun <light_v@163.com>
2017-08-12 03:25:28 -0700

    为vue提供接口数据

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,23 @@ const router = require('koa-router')()
 router.prefix('/admin')
 
 router.get('/info', function (ctx, next) {
-  ctx.body = {
-    
-  }
+	ctx.body = {
+		status: 1,
+		data: {
+			id: 0,
+			avatar: "6c224f4a20a446237a8e58de9822720e0df3d7c2.jpg",
+			user_name: "杀生丸",
+			created_at: "1348-02-23",
+			admin: 1
+		}
+	}
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+router.post('/login', function (ctx, next) {
+	console.log(ctx.request.body)
+	ctx.body = {
+		status: 1
+	}
 })
 
 module.exports = router
