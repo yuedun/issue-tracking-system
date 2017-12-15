892f11d4ed468e208d0566f4edfee64640dff3a6
yuedun <light_v@163.com>
2017-12-15 11:28:01 +0800

    修改自动注册路由路径获取，整理无用的依赖

--- utils/auto-register-routes.ts
+++ utils/auto-register-routes.ts
@@ -1,14 +1,15 @@
 /**
  * 项目根目录下的routes目录下的文件自动注册，无需在app.ts中手动注册路由
+ * 暂时不考虑多层级route目录，如果有可通过递归实现
  */
 import * as Koa from 'koa';
 import * as Fs from 'fs';
 import * as Path from 'path';
 
 export function registerRoute(app: Koa){
-    const files = Fs.readdirSync(Path.join(process.cwd(), "dest/routes"));
+    const files = Fs.readdirSync(Path.join(__dirname, "../routes"));
     for (let name of files) {
-        let fileName = Path.join(process.cwd(), "dest/routes", name);
+        let fileName = Path.join(__dirname, "../routes", name);
         if (Fs.statSync(fileName).isFile() && /\.js$/.test(fileName)) {
             let route = require(fileName);
             app.use(route.default.routes());
