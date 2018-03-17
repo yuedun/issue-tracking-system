3a766e6b7025cf19f21aded4d4319825e79e10ab
yuedun <light_v@163.com>
2018-03-17 20:32:17 +0800

    删除文件

--- models/users-model.ts
+++ models/users-model.ts
@@ -12,15 +12,15 @@ export interface ModelInstance
     //继承的第二个ModelAttributes如果省略会造成实例无法访问到属性
 };
 /**
- * 功能列表，只是提供一个可以选择的列表
+ * 用户表，该表为实际业务人员信息表
  */
 var Model = sequelize.define<ModelInstance, ModelAttributes>(
     'Users', {
-        user_name: Sequelize.STRING,
-        mobile: Sequelize.STRING
+        user_name: Sequelize.STRING(10),
+        mobile: Sequelize.STRING(11)
     }, {
         underscored: true,
-        tableName: 'users',
+        tableName: 'user',
         charset: 'utf8',
         collate: 'utf8_unicode_ci'
     }
