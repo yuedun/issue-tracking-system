dfe0d4b15b739822cee01502785e192e5296b73b
hale <hale.huo@zhangmen.com>
2019-03-15 17:53:06 +0800

    update dep

--- routes/other.ts
+++ routes/other.ts
@@ -7,17 +7,17 @@ class Controller {
 			body: "<h1>其他系统</h1>"
 		})
 	};
-	
+
 	public async a(ctx: any) {
 		ctx.body = 'koa2 string'
 	}
-	
+
 	public async b(ctx: any) {
 		ctx.body = {
 			title: 'koa2 json'
 		}
 	}
-	
+
 	public async c(ctx: any) {
 		const data = await rf('npm-debug.log');
 		await ctx.render('test', { title: 'Koa2-Easy', data: data })
